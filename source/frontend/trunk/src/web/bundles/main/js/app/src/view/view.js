e88c987b2497cd103f38511ed44749e9f8c66ada
elazarte <elazarte@031bca70-6652-11e2-a7d2-85a9c23f61f4>
2013-04-17 18:57:57 +0000

    *Logout: Se agregó redireccion a localhost

--- source/frontend/trunk/src/web/bundles/main/js/app/src/view/view.js
+++ source/frontend/trunk/src/web/bundles/main/js/app/src/view/view.js
@@ -194,6 +194,7 @@ App.ContentView = Ember.View.extend({
 	logout: function () {
 		App.get('userController').set('user', null);
 		localStorage.setObject('user', null);
+		document.location.href = 'http://localhost/';
 	},
 
 	didInsertElement: function () {
