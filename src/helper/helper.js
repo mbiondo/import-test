a68482e7983857859d1c1840b6fba65e4b7b766d
elazarte <elazarte@031bca70-6652-11e2-a7d2-85a9c23f61f4>
2013-09-27 18:36:03 +0000

    Envios a Archivo *Se modificó el link a ver archivo

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -11,7 +11,7 @@ Handlebars.registerHelper("linkEnvio", function(id, options) {
   var context = (options.contexts && options.contexts[0]) || this;
   var id = getPath(context, id, options.fn);
 
-  return "#/expedientesArchivados/envios/archivo/"+id+"/ver/";
+  return "#/envios/envio/"+id+"/ver/";
 });
 
 Handlebars.registerHelper("linkConfirmarEnvio", function(id, options) {
