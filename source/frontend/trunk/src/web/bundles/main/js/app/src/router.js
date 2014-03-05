144d2b56fae7aeee8ccf155d68cfac2b28986e06
elazarte <elazarte@031bca70-6652-11e2-a7d2-85a9c23f61f4>
2014-03-05 15:23:12 +0000

    *Se adicionó una mejora en los permisos

--- source/frontend/trunk/src/web/bundles/main/js/app/src/router.js
+++ source/frontend/trunk/src/web/bundles/main/js/app/src/router.js
@@ -84,6 +84,26 @@ App.Router =  Em.Router.extend({
 	//		_self.transitionTo("page403");
 	//	 }
 	  //});
+		var tienePermisos = false;
+
+		roles.forEach(function (rolesRequiered_value, rolesRequiered_index){
+	      var groupIsValid = true;
+				rolesRequiered_value.forEach(function(rolRequiered_value, rolRequiered_index){
+					if (!userRoles.contains(rolRequiered_value))
+					{
+						groupIsValid = false;
+					}
+				});
+
+	      if (groupIsValid) {
+	        tienePermisos = true;
+	      }
+		});
+
+	  if (!tienePermisos)
+	  {
+	  	_self.transitionTo("page403");
+	  }
 	},	
 	
 	root: Em.Route.extend({
