46386f00abc08c5424da47e5cae7ebcb6455537d
elazarte <elazarte@031bca70-6652-11e2-a7d2-85a9c23f61f4>
2014-04-15 15:33:46 +0000

    ProyectoQuery *Se removio codigo innecesario

--- src/model/model.js
+++ src/model/model.js
@@ -2032,13 +2032,9 @@ App.ProyectoQuery = Em.Object.extend({
 	useApi: false,
 	url: 'search',
 
-	firmante: '',
 	comision: '',
 	expdip: '',
 	tipo: '',
-	nombre: '',
-	usuario: '',
-	archivado: false,
 	editable: true,
 	dirty: false,
 	palabra: '',
@@ -2046,57 +2042,19 @@ App.ProyectoQuery = Em.Object.extend({
 	comisionesObject: [],
 
 	parametizable: [
-		'firmante',
 		'comision',
-		'fechaPublicacionHasta',
-		'fechaPublicacionDesde',
 		'expdip',
 		'tipo',
-		'archivado',
 		'palabras',
 	],
 
 	serializable: [
 		'id',
-		'firmante',
 		'comisiones',
-		'fechaPublicacionHasta',
-		'fechaPublicacionDesde',
 		'expdip',
 		'tipo',
-		'nombre',
-		'usuario',
-		'archivado',
 		'palabras',
 	],
-
-	normalize: function () {
-		if (this.get('fechaPublicacionHasta'))
-			this.set('fechaPublicacionHasta', moment(this.get('fechaPublicacionHasta'), 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss'));
-		if (this.get('fechaPublicacionDesde'))
-			this.set('fechaPublicacionDesde', moment(this.get('fechaPublicacionDesde'), 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss'));		
-	},
-
-	desNormalize: function () {
-		if (this.get('fechaPublicacionHasta'))
-			this.set('fechaPublicacionHasta', moment(this.get('fechaPublicacionHasta'), 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY'));
-		if (this.get('fechaPublicacionDesde'))
-			this.set('fechaPublicacionDesde', moment(this.get('fechaPublicacionDesde'), 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY'));	
-	},
-
-	parameters: function () {
-		var queryParameters = "";
-		this.normalize();
-		_self = this;
-		this.get('parametizable').forEach(function (item) {
-			if (_self.get(item)) {
-				queryParameters += "&" + item + "=" + _self.get(item);
-			}
-		});
-		this.desNormalize();
-		return queryParameters;
-
-	}.property('firmante', 'comision', 'fechaPublicacionDesde', 'fechaPublicacionHasta', 'expdip', 'tipo', 'archivado', 'expdip', 'palabras'),
 
 	comisiones: function () {
 		return $.map(this.get('comisionesObject'), function(value, key){ return value.get('nombre') });
