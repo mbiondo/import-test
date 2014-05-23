78606372a13defaba69fb92aef82331e868d88bb
elazarte <elazarte@031bca70-6652-11e2-a7d2-85a9c23f61f4>
2014-05-23 13:30:58 +0000

    Se cambio los roles de mesa de entrada

--- source/frontend/trunk/src/web/bundles/main/js/app/main.js
+++ source/frontend/trunk/src/web/bundles/main/js/app/main.js
@@ -1,6 +1,9 @@
 App.apiController = App.ApiController.create({
 	url: 'http://sparl-desa.hcdn.gob.ar:9090/sparl/rest/',
 
+
+
+
 	//url: 'http://10.120.8.67:8080/sparl/rest/',
 	tomcat: 'http://sparl-desa.hcdn.gob.ar:9090',
 
@@ -121,7 +124,7 @@ App.menuController = App.MenuController.create({
 					id: 2,
 					titulo: 'Mesa de Entrada',
 					url: '',
-					roles: [['ROLE_USER', 'ROLE_MESA_DE_ENTRADA_EDIT'], ['ROLE_SEC_PARL_VIEW']],
+					roles: [['ROLE_USER', 'ROLE_MESA_DE_ENTRADA'], ['ROLE_SEC_PARL_VIEW']],
 					subMenu: [
 						App.MenuItem.create({
 							id: 3,
